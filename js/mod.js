254b9fd9c97bc34ca1da73290fb77295cfdf7b98
EmiPiplu <62481444+EmiPiplu@users.noreply.github.com>
2021-02-25 17:06:43 +0000

    help

--- js/mod.js
+++ js/mod.js
@@ -1,21 +1,27 @@
 let modInfo = {
-	name: "The Not So Random Tree",
-	id: "RandomnessTree12984",
-	author: "EmiPiplu",
-	pointsName: "Instabilities",
+	name: "The ??? Tree",
+	id: "mymod",
+	author: "nobody",
+	pointsName: "points",
 	discordName: "",
 	discordLink: "",
-	changelogLink: "https://github.com/Acamaeda/The-Modding-Tree/blob/master/changelog.md",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	
-	offlineLimit: 0,  // In hours
+	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.3",
-	name: `Cubism`,
+	num: "0.0",
+	name: "Literally nothing",
 }
+
+let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.0</h3><br>
+		- Added things.<br>
+		- Added stuff.`
+
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
@@ -36,22 +42,11 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if(hasUpgrade("ss", 11)) gain = gain.mul(2)
-	if(hasUpgrade("ss", 21)) gain = gain.mul(2)
-	if(hasUpgrade("ss", 12)) gain = gain.mul(upgradeEffect("ss",12))
-	if(hasUpgrade("ss", 23)) gain = gain.mul(upgradeEffect("ss",23))
-	if(hasUpgrade("ss", 22)) gain = gain.pow(1.2)
-	gain = gain.mul(layers.um.effect())
-	if(hasUpgrade("ss", 33)) gain = gain.mul(2)
-	gain = gain.mul(buyableEffect("ss", 11)).add(1)
-	
 	return gain
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
 function addedPlayerData() { return {
-
-	
 }}
 
 // Display extra things at the top of the page
@@ -69,5 +64,10 @@ function isEndgame() {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600000) // Default is 1 hour which is just arbitrarily large
+	return(3600) // Default is 1 hour which is just arbitrarily large
+}
+
+// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
+// you can cap their current resources with this.
+function fixOldSave(oldVersion){
 }
